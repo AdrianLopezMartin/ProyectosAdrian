580689832693253de62179429ca8a8cc1b0b0898
AdrianLopezMartin <137994336+AdrianLopezMartin@users.noreply.github.com>
2023-10-24 09:35:59 +0200

    Add files via upload
    # Descripción del Proyecto
    
    Este repositorio contiene un pequeño proyecto diseñado para gestionar las asignaciones de tareas semanales y facilitar la comunicación a través del correo electrónico.
    
    ## Contenido del Repositorio
    
    - **ProgramacionTareasAdrian.js**: Este archivo contiene un programa en JavaScript que se encarga de la asignación de tareas específicas a Adrián.
    
    - **ProgramacionTareasSandra.js**: En este archivo, encontrarás un programa similar diseñado para configurar y asignar tareas a Sandra.
    
    - **ConfiguracionTareasNodered.png**: Este archivo almacena una imagen que representa la configuración realizada en Node-RED para gestionar las tareas.
    
    - **ConfiguracionTareasNodeRed2.png**: Esta imagen muestra la distribución de nodos en Node-RED, que es parte de la configuración de tareas.
    
    Este proyecto tiene como objetivo facilitar la gestión de tareas y la comunicación eficiente para las asignaciones semanales.

---
+++ ProgramacionTareasSandra.js
@@ -0,0 +1,131 @@
+// Code added here will be run once
+// whenever the node is started.
+
+/////////////////////Definición de variables/////////////////////////////////
+
+var tiempo = msg.payload;
+var marcaDeTiempo = new Date(msg.payload);
+var numero = marcaDeTiempo.getDate();
+var mes = marcaDeTiempo.getMonth();
+var dia = marcaDeTiempo.getDay();
+var meses = [];
+var dias = [];
+var tareasSemanales = [];
+var tareasMensuales = [];
+var tareasQuincenales = [];
+var tareasBimensuales=[];
+var tareasAnuales=[];
+var todasTareasQuincenales = [];
+var todasTareasMensuales = [];
+var todasTareasAnuales = [];
+var todasTareasBimensuales=[];
+var todasTareasSemestrales=[];
+var tareasSemestrales=[];
+ 
+/////////////////////////////////////////////////////
+dias=['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+// Dias de cada mes 'Enero':31,'Febrero':29,'Marzo':31,'Abril':30,'Mayo':31,'Junio':30,'Julio':31,'Agosto':31,'Septiembre':30,'Octubre':31,'Noviembre':30,'Diciembre':31
+meses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+todasTareasBimensuales=['Limpiar Cristales'];
+tareasSemanales= ['Ordenar Casa(Modo Elsa)','Limpiar baño','Limpiar Polvo','Poner Conga abajo','Colada (Común)','Mudar Cama (Común)'];
+todasTareasQuincenales = ['Poner Conga arriba','Nevera'];
+todasTareasMensuales=['Limpiar Microondas','Limpiar Horno', 'FregarSuelos(Común)'];
+todasTareasAnuales=['Parrilla','Limpieza Paneles'];
+todasTareasSemestrales=['Ordenar Armarios','Limpieza Cocina a Fondo', 'Congelador', 'Despensa','Revision Medicamentos', 'Cortinas'];
+
+///////////////////////////Asignación Tareas/////////////////////////
+
+// Asignacion Tareas Quincenales
+if(numero % 2 == 0 && ['Octubre','Enero','Marzo','Junio','Julio','Septiembre'].includes(meses[mes]) || numero % 2 !== 0 && ['Febrero','Abril','Mayo','Agosto'].includes(meses[mes]) ){
+    tareasQuincenales=todasTareasQuincenales;
+    if( numero<=15 && ['Febrero','Abril','Junio','Agosto','Octubre','Diciembre'].includes(meses[mes])){tareasBimensuales=todasTareasBimensuales}
+
+}
+// Asignacion Tareas Mensuales
+else if(numero<=15 ){
+    // Aignación Resto tareas
+    if ( ['Febrero', 'Abril', 'Junio', 'Agosto', 'Octubre', 'Diciembre'].includes(meses[mes])) { tareasBimensuales = todasTareasBimensuales }
+    if ('Noviembre'==meses[mes]){
+        tareasSemestrales=[todasTareasSemestrales[0],todasTareasSemestrales[4]];
+        tareasAnuales= todasTareasAnuales[2]
+    }
+    if ('Diciembre'== meses[mes] ){
+        tareasSemestrales=todasTareasSemestrales[2]
+    }
+    if ('Enero'== meses[mes] ){
+        tareasSemestrales=todasTareasSemestrales[3]
+    }
+    if ('Abril'== meses[mes] ){
+        tareasSemestrales=[todasTareasSemestrales[0]]
+    if ('Marzo'== meses[mes] ){
+        tareasAnuales=todasTareasAnuales[0]
+    }
+    if ('Junio'== meses[mes] ){
+        tareasAnuales=todasTareasAnuales[1]
+        tareasSemestrales=[todasTareasSemestrales[2],todasTareasSemestrales[4]]
+    }
+    }
+    tareasMensuales=todasTareasMensuales.slice(0,2)}
+else if(numero>15){
+    tareasMensuales=todasTareasMensuales[3]
+    if ('Abril'== meses[mes] ){
+        tareasSemestrales=[todasTareasSemestrales[1]]}
+    if ('Noviembre'==meses[mes]){
+        tareasSemestrales=todasTareasSemestrales[1]}   
+}
+////////////////////////////HTML CORREO//////////////////////////////
+
+// Construir el fragmento de HTML
+var html = "<html><head><title>Información de la Fecha</title></head><body>";
+html += "<p>Fecha: " + numero + " de " + meses[mes] + " (" + dias[dia] + ")</p>";
+html += "<p> Buenos días Sandra, aquí tienes tus tareas:</p>"
+html += "<p>Tareas Semanales:</p>";
+html += "<ul>";
+for (var i = 0; i < tareasSemanales.length; i++) {
+    html += "<li>" + tareasSemanales[i] + "</li>";
+}
+html += "</ul>";
+
+html += "<p>Tareas Quincenales:</p>";
+html += "<ul>";
+for (var i = 0; i < tareasQuincenales.length; i++) {
+    html += "<li>" + tareasQuincenales[i] + "</li>";
+}
+html += "</ul>";
+
+html += "<p>Tareas Mensuales:</p>";
+html += "<ul>";
+for (var i = 0; i < tareasMensuales.length; i++) {
+    html += "<li>" + tareasMensuales[i] + "</li>";
+}
+html += "</ul>";
+
+html += "<p>Tareas Bimensuales:</p>";
+html += "<ul>";
+for (var i = 0; i < tareasBimensuales.length; i++) {
+    html += "<li>" + tareasBimensuales[i] + "</li>";
+}
+html += "</ul>";
+
+html += "<p>Tareas Semestrales:</p>";
+html += "<ul>";
+for (var i = 0; i < tareasSemestrales.length; i++) {
+    html += "<li>" + tareasSemestrales[i] + "</li>";
+}
+html += "</ul>";
+
+
+html += "<p>Tareas Anuales:</p>";
+html += "<ul>";
+for (var i = 0; i < tareasAnuales.length; i++) {
+    html += "<li>" + tareasAnuales[i] + "</li>";
+}
+html += "</ul>";
+
+html += "<p> Disfruta del finde semana :) </p>"
+html += "</body></html>";
+
+// Asignar el fragmento HTML a msg.payload
+msg.payload = html;
+
+return msg;
